ccdfcded15728aa067cf546e3a6f9f2963cefa74
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-07-08 14:22:37 +0100

    coverage

--- tests/main_test.py
+++ tests/main_test.py
@@ -82,6 +82,13 @@ def test_violations(content: str) -> None:
         "def foo(a: None = ...) -> int: ...\n"
         "def foo(a: bool | None = None) -> None | int: ...\n",
         'def foo(a): return 1',
+        "from typing import Literal\n"
+        "@overload\n"
+        "def foo(a: Literal[True]) -> None: ...\n"
+        "@overload\n"
+        "def foo(a: Literal[False]) -> int: ...\n"
+        "def foo(a: bool = False) -> None | int: ...\n"
+        "def foo(b: bool = False) -> None | int: ...\n",
     ],
 )
 def test_passing(content: str) -> None:
